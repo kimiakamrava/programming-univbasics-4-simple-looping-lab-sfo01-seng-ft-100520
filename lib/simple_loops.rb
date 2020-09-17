a2a510de635a7b5a2a45faa1478ebab94a5f687d
kimia kamrava <kimiakamrava93@gmail.com>
2020-09-17 17:18:47 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -24,3 +24,12 @@ def output_array(  array = ["Be yourself;", "everyone else is already taken", "-
   end
 end
 
+def return_string_array(array)
+  count = 0
+  new_array = []
+  while count < array.length do
+    new_array.push(array[count].to_s) # OR new_array << array[count].to_s
+    count += 1
+  end
+  new_array
+end
