a39e19371bb68a946461ed96513123af3f21d2f8
kimia kamrava <kimiakamrava93@gmail.com>
2020-09-17 17:02:31 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -8,3 +8,10 @@ def loop_message_n_times(message= "Hello world.", limit = 5)
 end
 
 
+def loop_message_five_times(message= "Hello world.")
+  count = 0
+  while count < 5 do
+    puts message
+    count += 1
+  end
+end
